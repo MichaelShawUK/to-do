6a1fb4da6b9d1d9547f947a6ea71b4667c94c9b7
MichaelShawUK <MichaelShawUK@gmail.com>
2022-10-13 22:06:49 +0100

    Create unique projects

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import test from './test.js';
 
 test();
 
+const projects = new Set();
+
+function initializeListeners() {
+  openTaskModalListener();
+  hideTaskModalListener();
+  addTaskListener();
+  clickOutsideModal();
+  createProjectListener();
+}
+
 function openTaskModalListener() {
   const createTask = document.querySelector('.create-task');
   createTask.onclick = displayTaskModal;
@@ -14,13 +24,17 @@ function hideTaskModalListener() {
 }
 
 function displayTaskModal() {
-  const addTaskModal = document.getElementById('add-task-modal');
+  const addTaskModal = document.querySelector('.modal-container');
+  const closeModal = document.getElementById('close-modal');
   addTaskModal.style.display = 'block';
+  createProjectOptions();
+  closeModal.onclick = hideTaskModal;
 }
 
 function hideTaskModal() {
-  const addTaskModal = document.getElementById('add-task-modal');
+  const addTaskModal = document.querySelector('.modal-container');
   addTaskModal.style.display = 'none';
+  clearTaskModal();
 }
 
 function addTaskListener() {
@@ -33,19 +47,32 @@ function getTask() {
   const taskDueDate = document.getElementById('task-due-date');
   const taskPriority = document.getElementById('task-priority');
   const taskDescription = document.getElementById('task-description');
+  const taskProject = document.getElementById('task-project');
 
   let task = {
     title: taskTitle.value,
     dueDate: taskDueDate.value,
     isPriority: taskPriority.checked,
     description: taskDescription.value,
+    project: taskProject.value,
   }
   hideTaskModal();
-  addTask(task);
+  if (task.title && task.dueDate) addTask(task);
+  
+}
+
+function clearTaskModal() {
+  const taskTitle = document.getElementById('task-title');
+  const taskDueDate = document.getElementById('task-due-date');
+  const taskPriority = document.getElementById('task-priority');
+  const taskDescription = document.getElementById('task-description');
+  const taskProject = document.getElementById('task-project');
+
   taskTitle.value = null;
   taskDueDate.value = null;
   taskPriority.checked = null;
   taskDescription.value = null;
+  taskProject.value = null;
 }
 
 function addTask(task) {
@@ -66,11 +93,72 @@ function addTask(task) {
 
   taskNode.append(checkBtn, taskNodeTitle, taskNodeDueDate, deleteBtn);
   taskView.append(taskNode);
-
-
-  
 }
 
-openTaskModalListener();
-hideTaskModalListener();
-addTaskListener();
+function clickOutsideModal() {
+  const modalContainer = document.querySelector('.modal-container');
+  modalContainer.onclick = e => {
+    if (e.target.parentElement.tagName === 'BODY') hideTaskModal();
+  }
+}
+
+function createProjectListener() {
+  const createProjectBtn = document.getElementById('create-project-btn');
+  createProjectBtn.onclick = createProject;
+}
+
+function createProject() {
+  const projectsNode = document.getElementById('projectsNode');
+  const addProjectBtn = document.createElement('button');
+  addProjectBtn.setAttribute('id', 'add-project-btn');
+  const newProject = document.createElement('input');
+  newProject.setAttribute('type', 'text');
+  newProject.setAttribute('id', 'new-project');
+  addProjectBtn.append('ADD PROJECT');
+  projectsNode.append(newProject, addProjectBtn);
+  removeProjectListener();
+  addProject();
+}
+
+function removeProjectListener() {
+  const createProjectBtn = document.getElementById('create-project-btn');
+  createProjectBtn.onclick = null;
+}
+
+function addProject() {
+  const projectsNode = document.getElementById('projectsNode');
+  const newProject = document.getElementById('new-project');
+  const addProjectBtn = document.getElementById('add-project-btn');
+  addProjectBtn.addEventListener('click', () => {
+    if (newProject.value && !(projects.has(newProject.value))) {
+      projects.add(newProject.value);
+      const projectNode = document.createElement('div');
+      projectNode.append(`/ / ${newProject.value}`);
+      projectsNode.append(projectNode);
+    }
+    newProject.remove();
+    addProjectBtn.remove();
+    createProjectListener();
+  })
+}
+
+function getOptions() {
+  const optionNodes = document.querySelectorAll('option');
+  let existingOptions = [];
+  Array.from(optionNodes).forEach(option => existingOptions.push(option.innerText));
+  return existingOptions;
+}
+
+function createProjectOptions() {
+  const taskProject = document.getElementById('task-project');
+  const options = getOptions();
+  projects.forEach(project => {
+    if (!options.includes(project)) {
+      let projectOption = document.createElement('option');
+      projectOption.append(project);
+      taskProject.append(projectOption);
+    }
+  })
+}
+
+initializeListeners();

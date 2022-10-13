f22a9d6132e7dbd23e57b6dcfbde7b001419f4a2
MichaelShawUK <MichaelShawUK@gmail.com>
2022-10-13 16:06:53 +0100

    Allow user to add tasks

--- src/index.js
+++ src/index.js
@@ -1,6 +1,76 @@
 import './style.css';
-import './test.js';
 import test from './test.js';
 
-document.body.style.background = 'red';
 test();
+
+function openTaskModalListener() {
+  const createTask = document.querySelector('.create-task');
+  createTask.onclick = displayTaskModal;
+}
+
+function hideTaskModalListener() {
+  const cancelTaskModal = document.getElementById('cancel-task-btn');
+  cancelTaskModal.onclick = hideTaskModal;
+}
+
+function displayTaskModal() {
+  const addTaskModal = document.getElementById('add-task-modal');
+  addTaskModal.style.display = 'block';
+}
+
+function hideTaskModal() {
+  const addTaskModal = document.getElementById('add-task-modal');
+  addTaskModal.style.display = 'none';
+}
+
+function addTaskListener() {
+  const addTaskBtn = document.getElementById('add-task-btn');
+  addTaskBtn.onclick = getTask;
+}
+
+function getTask() {
+  const taskTitle = document.getElementById('task-title');
+  const taskDueDate = document.getElementById('task-due-date');
+  const taskPriority = document.getElementById('task-priority');
+  const taskDescription = document.getElementById('task-description');
+
+  let task = {
+    title: taskTitle.value,
+    dueDate: taskDueDate.value,
+    isPriority: taskPriority.checked,
+    description: taskDescription.value,
+  }
+  hideTaskModal();
+  addTask(task);
+  taskTitle.value = null;
+  taskDueDate.value = null;
+  taskPriority.checked = null;
+  taskDescription.value = null;
+}
+
+function addTask(task) {
+  const taskView = document.getElementById('task-view');
+  const taskNode = document.createElement('div');
+  taskNode.setAttribute('class', 'task');
+
+  const checkBtn = document.createElement('button');
+  checkBtn.append('#');
+  const deleteBtn = document.createElement('button');
+  deleteBtn.append('X');
+  const taskNodeTitle = document.createElement('div');
+  taskNodeTitle.setAttribute('class', 'task-title');
+  taskNodeTitle.append(task.title);
+  const taskNodeDueDate = document.createElement('div');
+  taskNodeDueDate.setAttribute('class', 'task-due-date');
+  taskNodeDueDate.append(task.dueDate);
+
+  taskNode.append(checkBtn, taskNodeTitle, taskNodeDueDate, deleteBtn);
+  taskView.append(taskNode);
+
+
+  
+}
+
+openTaskModalListener();
+hideTaskModalListener();
+addTaskListener();
